0f591390c0e5e0df9c1ad861d86158d151d42a8c
Garrett Priddy <Kaboom242@gmail.com>
2022-11-17 14:24:25 -0800

    Webworkers fast

--- htmlRequest.js
+++ htmlRequest.js
@@ -1,14 +1,15 @@
 onmessage = function (e) {
 	//url, id1, id2
+	//console.log(e.data);
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function () {
 		if (request.readyState == 4 && request.status == 200) {
+			console.log(e[0]);
 			//callback(request.responseText); // Another callback here
 			postMessage(request.responseText);
-			
 		}
 	}
-	request.open('HEAD', e[0], false);
+	request.open('HEAD', e.data, false);
 	request.send();
 	
 }
